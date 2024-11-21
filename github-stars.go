c1f7086348195eca5820f2fe1f2bb509b29c932f
Christophe Lambin <christophe.lambin@gmail.com>
2024-11-21 01:28:56 +0100

    feat: implement as a GitHub App

--- github-stars.go
+++ github-stars.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
-	"github.com/clambin/github-stars/internal/github"
 	"github.com/clambin/github-stars/internal/server"
-	"github.com/clambin/github-stars/internal/store"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log/slog"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -18,15 +13,7 @@ import (
 var (
 	version = "change-me"
 
-	debug           = flag.Bool("debug", false, "Enable debug mode")
-	githubToken     = flag.String("github.token", "", "GitHub API githubToken")
-	webHookAddr     = flag.String("github.webhook.addr", ":8080", "Address for the webhook server")
-	webHookSecret   = flag.String("github.webhook.secret", "todo", "Secret for the webhook server")
-	addr            = flag.String("addr", ":9091", "Prometheus handler address")
-	slackWebHook    = flag.String("slack.webHook", "", "Slack WebHook URL")
-	directory       = flag.String("directory", ".", "Database directory")
-	user            = flag.String("user", "", "GitHub username")
-	includeArchived = flag.Bool("include_archived", false, "Include archived repositories")
+	debug = flag.Bool("debug", false, "Enable debug mode")
 )
 
 func main() {
@@ -41,46 +28,8 @@ func main() {
 	}
 	l := slog.New(slog.NewTextHandler(os.Stderr, &opts))
 
-	l.Info("github-stars is starting", "version", version)
-
-	http.Handle("/metrics", promhttp.Handler())
-	go func() {
-		if err := http.ListenAndServe(*addr, nil); !errors.Is(err, http.ErrServerClosed) {
-			l.Warn("failed to start Prometheus handler", "err", err)
-		}
-	}()
-
-	notifiers := server.Notifiers{
-		server.SLogNotifier{Logger: l},
-	}
-	if *slackWebHook != "" {
-		notifiers = append(notifiers, &server.SlackWebHookNotifier{WebHookURL: *slackWebHook, Logger: l})
-	}
-
-	client := github.NewGitHubClient(*githubToken)
-	repoStore := store.New(*directory)
-	if err := repoStore.Load(); err != nil {
-		l.Warn("database not found. possibly this is a new installation", "err", err)
-	}
-
-	l.Info("scanning repositories")
-
-	// Scan repos and populate the database. This catches up any stars given while we were down.
-	err := server.Scan(ctx, *user, client, repoStore, notifiers, *includeArchived, l.With("component", "scanner"))
-	if err != nil {
-		l.Error("failed to scan github repositories", "err", err)
-	}
-
-	l.Info("starting webhook handler")
-
-	h := server.GitHubAuth(*webHookSecret)(
-		&server.Webhook{
-			Store:  repoStore,
-			Logger: l.With("component", "webhook"),
-		},
-	)
-
-	if err = http.ListenAndServe(*webHookAddr, h); !errors.Is(err, http.ErrServerClosed) {
-		l.Error("failed to start WebHook handler", "err", err)
+	if err := server.Run(ctx, version, l); err != nil {
+		l.Error("server failed", "err", err)
+		os.Exit(1)
 	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,8 +2,27 @@ package server
 
 import (
 	"context"
+	"errors"
+	"flag"
+	ghc "github.com/clambin/github-stars/internal/github"
+	"github.com/clambin/github-stars/internal/store"
 	"github.com/google/go-github/v66/github"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"iter"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+var (
+	githubToken     = flag.String("github.token", "", "GitHub API githubToken")
+	webHookAddr     = flag.String("github.webhook.addr", ":8080", "Address for the webhook server")
+	webHookSecret   = flag.String("github.webhook.secret", "todo", "Secret for the webhook server")
+	addr            = flag.String("addr", ":9091", "Prometheus handler address")
+	slackWebHook    = flag.String("slack.webHook", "", "Slack WebHook URL")
+	directory       = flag.String("directory", ".", "Database directory")
+	user            = flag.String("user", "", "GitHub username")
+	includeArchived = flag.Bool("include_archived", false, "Include archived repositories")
 )
 
 type Client interface {
@@ -19,4 +38,71 @@ type Store interface {
 
 type Notifier interface {
 	Notify(repository *github.Repository, gazers []*github.Stargazer)
+}
+
+func Run(ctx context.Context, version string, l *slog.Logger) error {
+	client := ghc.NewGitHubClient(*githubToken)
+	return runWithClient(ctx, client, version, l)
+}
+
+func runWithClient(ctx context.Context, client Client, version string, l *slog.Logger) error {
+	l.Info("github-stars is starting", "version", version)
+
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		if err := http.ListenAndServe(*addr, nil); !errors.Is(err, http.ErrServerClosed) {
+			l.Warn("failed to start Prometheus handler", "err", err)
+		}
+	}()
+
+	notifiers := Notifiers{
+		SLogNotifier{Logger: l},
+	}
+	if *slackWebHook != "" {
+		notifiers = append(notifiers, &SlackWebHookNotifier{WebHookURL: *slackWebHook, Logger: l})
+	}
+
+	repoStore := store.New(*directory)
+	if err := repoStore.Load(); err != nil {
+		l.Warn("database not found. possibly this is a new installation", "err", err)
+	}
+
+	l.Info("scanning repositories")
+
+	// Scan repos and populate the database. This catches up any stars given while we were down.
+	err := Scan(ctx, *user, client, repoStore, notifiers, *includeArchived, l.With("component", "scanner"))
+	if err != nil {
+		l.Error("failed to scan github repositories", "err", err)
+	}
+
+	l.Info("starting webhook handler")
+
+	httpServer := &http.Server{
+		Addr: *webHookAddr,
+		Handler: GitHubAuth(*webHookSecret)(
+			&Webhook{
+				Store:  repoStore,
+				Logger: l.With("component", "webhook"),
+			},
+		),
+	}
+
+	errCh := make(chan error)
+	go func() {
+		err = httpServer.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			err = nil
+		}
+		errCh <- err
+	}()
+
+	select {
+	case err = <-errCh:
+		return err
+	case <-ctx.Done():
+	}
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+	return httpServer.Shutdown(shutdownCtx)
 }
